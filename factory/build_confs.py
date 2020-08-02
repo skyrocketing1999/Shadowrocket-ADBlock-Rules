c466cdba72914ffd9a0981c9780895ca4d662792
skyrocketingHong <null@skyrocketing.ninja>
2020-08-02 12:41:30 +0800

    优化规则

--- factory/build_confs.py
+++ factory/build_confs.py
@@ -65,8 +65,8 @@ values['manual_direct'] = getRulesStringFromFile('manual_direct.txt', 'Direct')
 values['manual_proxy']  = getRulesStringFromFile('manual_proxy.txt', 'PROXY,force-remote-dns')
 values['manual_reject'] = getRulesStringFromFile('manual_reject.txt', 'Reject')
 
-values['gfwlist'] = getRulesStringFromFile('resultant/gfw.list', 'PROXY,force-remote-dns') \
-                  + getRulesStringFromFile('manual_gfwlist.txt', 'PROXY,force-remote-dns')
+values['gfwlist'] = getRulesStringFromFile('resultant/gfw.list', 'Proxy,force-remote-dns') \
+                  + getRulesStringFromFile('manual_gfwlist.txt', 'Proxy,force-remote-dns')
 
 
 # make confs
